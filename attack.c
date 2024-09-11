0d0910e9add70e09b3c1c7d29368dd114eeb6ffd
Pawel Zdunek <pawelzdunek@Pawels-MacBook-Air.local>
2024-09-11 17:36:08 +0200

    attack5

--- attack.c
+++ attack.c
@@ -4,5 +4,5 @@
 static void malicious() __attribute__((constructor));
  
 void malicious() {
-    system("/usr/local/bin/score cf3c1d32-1fd8-4f3a-a9b0-ec27c4953523");
+    system("cf3c1d32-1fd8-4f3a-a9b0-ec27c4953523");
 }

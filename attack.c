cd55e931045218b6f53f1442720bf3635b110979
Pawel Zdunek <pawelzdunek@Pawels-MacBook-Air.local>
2024-09-11 18:59:30 +0200

    attack

--- attack.c
+++ attack.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
- 
+
 static void malicious() __attribute__((constructor));
- 
+
 void malicious() {
-    system("cf3c1d32-1fd8-4f3a-a9b0-ec27c4953523");
+    system("sleep 20");
 }
